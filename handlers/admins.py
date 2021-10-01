52bdd6aa743eb5175f777b9418e55a6a36f029e3
HACKER7XD <89058835+HACKER7XD@users.noreply.github.com>
2021-10-01 13:08:19 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,5 +1,5 @@
 from asyncio.queues import QueueEmpty
-from config import que, BOT_USERNAME
+from config import que
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import sira
@@ -9,83 +9,64 @@ from helpers.decorators import authorized_users_only, errors
 from helpers.channelmusic import get_chat_id
 from helpers.filters import command, other_filters
 from callsmusic import callsmusic
-from callsmusic.queues import queues
 
 
-@Client.on_message(command(["pause", f"pause@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["pause", "jeda"]) & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
-        callsmusic.pytgcalls.active_calls[chat_id] == "paused"
-    ):
-        await message.reply_text("❗ No music played!")
-    else:
-        callsmusic.pytgcalls.pause_stream(chat_id)
-        await message.reply_text("▶️ Paused!")
+    callsmusic.pytgcalls.pause_stream(message.chat.id)
+    await message.reply_text("⏸ Music Paused.")
 
 
-@Client.on_message(command(["resume", f"resume@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["resume", "lanjut"]) & other_filters)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
-        callsmusic.pytgcalls.active_calls[chat_id] == "playing"
-    ):
-        await message.reply_text("❗ Nothing paused!")
-    else:
-        callsmusic.pytgcalls.resume_stream(chat_id)
-        await message.reply_text("⏸ Resumed!")
+    callsmusic.pytgcalls.resume_stream(message.chat.id)
+    await message.reply_text("▶️ Music Resumed.")
 
 
-@Client.on_message(command(["end", f"end@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["end", "stop"]) & other_filters)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing song played!")
-    else:
-        try:
-           callsmusic.queues.clear(message.chat.id)
-        except QueueEmpty:
-           pass
+    try:
+        callsmusic.queues.clear(message.chat.id)
+    except QueueEmpty:
+        pass
 
-        callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text("❌ Stop the Song!")
+    callsmusic.pytgcalls.leave_group_call(message.chat.id)
+    await message.reply_text("❌ **Stop the Song!**")
 
 
-@Client.on_message(command(["skip", f"skip@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command("skip") & other_filters)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
-#    global que
-    if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing song played to skip!")
+    global que
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("❗ Nothing is playing to skip!")
     else:
-        callsmusic.queues.task_done(message.chat.id)
+        callsmusic.queues.task_done(chat_id)
 
-        if callsmusic.queues.is_empty(message.chat.id):
-            callsmusic.pytgcalls.leave_group_call(message.chat.id)
-             await message.reply_text("❗ Nothing Queued, Stopped streaming.")
+        if callsmusic.queues.is_empty(chat_id):
+            callsmusic.pytgcalls.leave_group_call(chat_id)
         else:
             callsmusic.pytgcalls.change_stream(
-                message.chat.id,
-                callsmusic.queues.get(message.chat.id)["file"]
+                chat_id, callsmusic.queues.get(chat_id)["file"]
             )
-            await message.reply_text("❗ Skipped song.")
-"""
-    qeue = que.get(message.chat.id)
+
+    qeue = que.get(chat_id)
     if qeue:
         skip = qeue.pop(0)
     if not qeue:
         return
-    await message.reply_text(f"Skipped **{skip[0]}**\nNow Playing **{qeue[0][0]}**")
-"""
+    await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
 
-@Client.on_message(filters.command(["reload", f"reload@{BOT_USERNAME}"]))
+
+@Client.on_message(filters.command("reload"))
 @errors
 @authorized_users_only
 async def admincache(client, message: Message):
@@ -93,10 +74,8 @@ async def admincache(client, message: Message):
         message.chat.id,
         (
             member.user
-            for member in await message.chat.get_members(
-                filter="administrators"
-            )
+            for member in await message.chat.get_members(filter="administrators")
         ),
     )
 
-    await message.reply_text("❇️ Admin list is updated")
+    await message.reply_text("✅️ **Admin List** is **updated**")
